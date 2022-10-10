a43220be99e47fcb3f2106abb774c2168fd9aecd
DiegoTW <89322414+DiegoTW@users.noreply.github.com>
2022-10-09 23:28:23 -0300

    fusion del menu en mobile más carrito de compras

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const navbarIconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const navbarShopCart = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
 
+navbarIconMenu.addEventListener('click', toggleNavbarIconMenu);
 navbarEmail.addEventListener('click', toggleDesktopMenu);
+navbarShopCart.addEventListener('click', toggleProductDetail);
+
+function toggleNavbarIconMenu() {
+  productDetail.classList.add('inactive');
+  mobileMenu.classList.toggle('inactive');
+}
 
 function toggleDesktopMenu() {
+  productDetail.classList.add('inactive');
   desktopMenu.classList.toggle('inactive');
+}
+
+function toggleProductDetail() {
+  desktopMenu.classList.add('inactive');
+  mobileMenu.classList.add('inactive');
+  productDetail.classList.toggle('inactive');
 }
